57bc2770b7436cad56274fc528f5eeb4cada3995
Zeen Li <118988701+zericli090725@users.noreply.github.com>
2025-10-11 19:53:50 -0700

    Deletion of Vite default files, started blank

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import { useState } from 'react'
 import gordonLogo from './assets/icon.png'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a  target="_blank">
-          <img src={gordonLogo} className="logo" alt="Gordon logo" />
-        </a>
-        <a  target="_blank">
-          <img src={gordonLogo} className="logo react" alt="Gordon logo" />
-        </a>
-      </div>
-      <h1>Gordon Lu</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Gordon Lu's personal website
-      </p>
-    </>
+    <div>
+      <h1>Hello, Gordon Lu!</h1>
+    </div>
   )
 }
 
