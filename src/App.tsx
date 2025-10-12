c7e5072026275f46d5681052b2ed6a4f1a16fa13
Zeen Li <118988701+zericli090725@users.noreply.github.com>
2025-10-11 19:22:13 -0700

    Changed home page images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import gordonLogo from './assets/icon.png'
 import './App.css'
 
 function App() {
@@ -9,11 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a  target="_blank">
+          <img src={gordonLogo} className="logo" alt="Gordon logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a  target="_blank">
+          <img src={gordonLogo} className="logo react" alt="Gordon logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
