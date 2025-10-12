64953990af927d3346696308aef47292c32372c1
Zeen Li <118988701+zericli090725@users.noreply.github.com>
2025-10-11 19:28:43 -0700

    Changed basic info of the page

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,14 @@ function App() {
           <img src={gordonLogo} className="logo react" alt="Gordon logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Gordon Lu</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Gordon Lu's personal website
       </p>
     </>
   )
